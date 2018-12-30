30429f267fb4956cc4bb4c5b1af5a75353736b9a
darckon <informatik.rojas@gmail.com>
2018-12-30 15:36:09 -0400

    Se añaden nuevas tablas y relaciones

--- escenario/admin.py
+++ escenario/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Escenario, Trama, Texto, Evento
+from .models import Escenario, Trama, Texto, Evento, Etapa, Tipo_Pregunta, Pregunta, Opciones_Pegunta
 
 # Register your models here.
 
@@ -7,3 +7,7 @@ admin.site.register(Escenario)
 admin.site.register(Trama)
 admin.site.register(Texto)
 admin.site.register(Evento)
+admin.site.register(Etapa)
+admin.site.register(Tipo_Pregunta)
+admin.site.register(Pregunta)
+admin.site.register(Opciones_Pegunta)
--- escenario/models.py
+++ escenario/models.py
@@ -19,13 +19,43 @@ class Trama(models.Model):
 class Texto(models.Model):
     nombre   = models.CharField('Nombre', max_length = 100)
     texto    = models.TextField('Descripcion')
-    contexto = models.ForeignKey(Trama, on_delete=models.PROTECT)
+    trama = models.ForeignKey(Trama, on_delete=models.PROTECT)
     def __str__(self):
-        return "%s | %s " % (self.nombre, self.contexto)
+        return "%s | %s " % (self.nombre, self.trama)
+
+class Etapa(models.Model):
+    nombre = models.CharField('Nombre' , max_length = 100)
+    inicio = models.DateField('Fecha Inicio', null = True)
+    fin    = models.DateField('Fecha Termino', null = True)
+    trama  = models.ForeignKey(Trama, on_delete=models.PROTECT)
+    def __str__(self):
+        return "%s | %s" % (self.nombre, self.trama)
 
 class Evento(models.Model):
     nombre = models.CharField('Nombre' , max_length = 100)
     fecha  = models.DateTimeField('Fecha Evento', null=True, blank=True)
+    etapa  = models.ForeignKey(Etapa, on_delete=models.PROTECT)
+    def __str__(self):
+        return "%s | %s " % (self.nombre, self.etapa)
+
+class Tipo_Pregunta(models.Model):
+    nombre = models.CharField('Nombre' , max_length = 100)
+    def __str__(self):
+        return "%s" % (self.nombre)
+
+class Opciones_Pegunta(models.Model):
+    nombre = models.CharField('Nombre' , max_length = 100)
     trama  = models.ForeignKey(Trama, on_delete=models.PROTECT)
     def __str__(self):
-        return "%s | %s " % (self.nombre, self.trama)
+        return "%s | %s" % (self.nombre, self.trama)
+
+class Pregunta(models.Model):
+    nombre        = models.CharField('Nombre' , max_length = 100)
+    descripcion   = models.TextField('Descripcion')
+    opciones      = models.ManyToManyField(Opciones_Pegunta)
+    tipo_pregunta = models.ForeignKey(Tipo_Pregunta, on_delete=models.PROTECT)
+    evento  = models.ForeignKey(Evento, null = True, blank = True, on_delete=models.PROTECT)
+    etapa   = models.ManyToManyField(Etapa,  null = True, blank = True )
+    def __str__(self):
+        return "%s | %s " % (self.nombre, self.tipo_pregunta)
+
